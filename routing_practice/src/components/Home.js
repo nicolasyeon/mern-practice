4c4226f49e9f8c03aaec1610fdb800a603eca30c
Nick Yeon <nkyeon0709@gmail.com>
2021-03-16 16:12:30 -0700

    Updated Assignment

--- routing_practice/src/components/Home.js
+++ routing_practice/src/components/Home.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-// const Home = props => {
-//     return (
-//         <div>
-//             Welcome!
-//         </div>
-//     )
-// }
-
 const Home = (props) => {
     console.log(props);
     if (!props.word) {
