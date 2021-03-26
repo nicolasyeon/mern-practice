cc8b9c5dcf9302936783108ca7469600a8ab339a
Nick Yeon <nkyeon0709@gmail.com>
2021-03-26 15:30:38 -0700

    Part I Product Manager completed

---
+++ product_manager/client/src/components/ProductForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+const ProductForm = () => {
+    //keep track of what is being typed via useState hook
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+    //handler when the form is submitted
+    const onSubmitHandler = e => {
+        //prevent default behavior of the submit
+        e.preventDefault();
+        //make a post request to create a new product
+        axios.post('http://localhost:8000/api/product', {
+            title,    // this is shortcut syntax for title: title,
+            price,
+            description      // this is shortcut syntax for description: description
+        })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+    //onChange to update Title, Price, Description
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>Title</label><br />
+                <input type="text" onChange={(e) => setTitle(e.target.value)} />
+            </p>
+            <p>
+                <label>Price</label><br />
+                <input type="text" onChange={(e) => setPrice(e.target.value)} />
+            </p>
+            <p>
+                <label>Description</label><br />
+                <input type="text" onChange={(e) => setDescription(e.target.value)} />
+            </p>
+            <input type="submit" />
+        </form>
+    )
+}
+export default ProductForm;
---
+++ product_manager/server/controllers/product.controller.js
@@ -0,0 +1,17 @@
+const Product = require('../models/product.model');    /* this is new */
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+/* The method below is new */
+module.exports.createProduct = (request, response) => {
+    const { title, price, description } = request.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(person => response.json(person))
+        .catch(err => response.json(err));
+}
